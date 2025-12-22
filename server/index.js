568fa0b615dd7d71a0b6a7bed9dcf8e722c8d4ba
Gokul <gokul@Gokuls-MacBook-Air.local>
2025-12-22 15:19:02 +0530

    Changes in CORS

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://sacl-consultancy.vercel.app'],
     credentials: true
 }))
 
