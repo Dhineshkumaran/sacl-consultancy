ddb92ccaade21d92c3482822c3c82d790d73c2b4
Dhinesh kumaran S <dhineshkumarans.22cse@kongu.edu>
2026-01-26 17:00:38 +0530

    Refactor mailSender to use nodemailer

--- server/src/utils/mailSender.js
+++ server/src/utils/mailSender.js
@@ -1,64 +1,35 @@
-// import nodemailer from 'nodemailer';
-// import CustomError from './customError.js';
-
-// const transporter = nodemailer.createTransport({
-//   host: process.env.SMTP_HOST,
-//   port: process.env.SMTP_PORT,
-//   secure: false,
-//   auth: {
-//     user: process.env.SMTP_USER,
-//     pass: process.env.SMTP_PASS
-//   },
-//   tls: {
-//     rejectUnauthorized: false
-//   }
-// });
-
-// const sendMail = ({ to, subject, text, html }) => {
-//   transporter.sendMail({ from: `"SACL Digital Trial Card" <${process.env.SMTP_USER}>`, to, subject, text, html }, (error, info) => {
-//     if (error) {
-//       throw new CustomError(error.message, error.statusCode || 500);
-//     }
-//   });
-// };
-
-// const sendMailToAllDepartments = async ({ to, subject, text, html }) => {
-//   to.forEach(email => {
-//     transporter.sendMail({ from: `"SACL Digital Trial Card" <${process.env.SMTP_USER}>`, to: email, subject, text, html }, (error, info) => {
-//       if (error) {
-//         throw new CustomError(error.message, error.statusCode || 500);
-//       }
-//     });
-//   });
-// }
-
-// export default sendMail;
-
-import { Resend } from 'resend';
+import nodemailer from 'nodemailer';
 import CustomError from './customError.js';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
 
-const transporter = {
-  sendMail: async ({ to, subject, text, html }) => {
-    try {
-      const { data, error } = await resend.emails.send({
-        from: process.env.EMAIL_FROM,
-        to: 'dhineshkumarans.22cse@kongu.edu',
-        subject,
-        text,
-        html,
-      });
+const sendMail = ({ to, subject, text, html }) => {
+  transporter.sendMail({ from: `"SACL Digital Trial Card" <${process.env.SMTP_USER}>`, to, subject, text, html }, (error, info) => {
+    if (error) {
+      throw new CustomError(error.message, error.statusCode || 500);
+    }
+  });
+};
 
+const sendMailToAllDepartments = async ({ to, subject, text, html }) => {
+  to.forEach(email => {
+    transporter.sendMail({ from: `"SACL Digital Trial Card" <${process.env.SMTP_USER}>`, to: email, subject, text, html }, (error, info) => {
       if (error) {
         throw new CustomError(error.message, error.statusCode || 500);
       }
-      return data;
-    } catch (err) {
-      if (err instanceof CustomError) throw err;
-      throw new CustomError(err.message, err.statusCode || 500);
-    }
-  }
-};
+    });
+  });
+}
 
-export default transporter.sendMail;
+export default sendMail;
